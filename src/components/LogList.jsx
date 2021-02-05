6f34d153fa7ee7957b4deea21a4ceb31ae565834
Yared <yarednegash3115@gmail.com>
2021-02-05 00:29:25 -0500

    display cards inline using bootstrap

--- src/components/LogList.jsx
+++ src/components/LogList.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import IntroJumbo from "./IntroJumbo"
 
 import LogCard from "./LogCard"
+import { Container, Row, Col } from "react-bootstrap";
 
 const test = { "logTitle": "The Man in the High Castle", "logPlot": "In a dystopian America dominated by Nazi Germany and Imperial Japan, a young woman discovers a mysterious film that may hold the key to toppling the totalitarian regimes.", "Release": "15 Jan 2015", "logPoster": "https://m.media-amazon.com/images/M/MV5BZWEwNzQ4NzUtMWRmYS00NDdiLTg5NDItODA5M2M4YTM0ZTE2XkEyXkFqcGdeQXVyMTAzNjU2NjM1._V1_SX300.jpg" };
 
@@ -16,16 +17,23 @@ function LogList(props) {
     }
 
     return (
-        <div>
-            {logArray.map((element) => {
-                return (<LogCard
-                    logTitle={element.logTitle}
-                    logPlot={element.logPlot}
-                    logReleaseDate={element.logReleaseDate}
-                    logPoster={element.logPoster}
-                />)
-            })}
-        </div>
+        <Container >
+            <Row>
+                {logArray.map((element) => {
+                    return (
+                        <Col sm={12} md={6} lg={4}>
+                            <LogCard
+                                logTitle={element.logTitle}
+                                logPlot={element.logPlot}
+                                logReleaseDate={element.logReleaseDate}
+                                logPoster={element.logPoster}
+                            />
+                        </Col>
+                        )
+                })}
+
+            </Row>
+        </Container>
     );
 }
 
