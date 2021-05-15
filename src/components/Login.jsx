fa746f65f195dfcf6cb814a5b636379af0199045
Yared <yarednegash3115@gmail.com>
2021-05-14 23:15:25 -0400

    set credentials paramter so front end can set cookies

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -14,6 +14,7 @@ function Login() {
             alert(`email ${emailState} and pass ${passwordState}`);
             const options = {
                 method: "POST",
+                credentials: 'include',
                 headers: {
                     'Content-Type': 'application/json',
                     Accept: 'application/json'
