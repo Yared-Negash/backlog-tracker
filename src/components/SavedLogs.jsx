11ecbca5ae0a4cf0266668a35522b06185e731d5
Yared <yarednegash3115@gmail.com>
2021-05-19 00:31:54 -0400

    show saved logs on home screen (if logged in)

--- src/components/SavedLogs.jsx
+++ src/components/SavedLogs.jsx
@@ -1,45 +1,69 @@
 import React, { useState, useEffect } from "react";
 import IntroJumbo from '../components/IntroJumbo'
+import { Container, Row, Col } from "react-bootstrap";
+
+import LogCard from "./LogCard"
 
 const location = process.env.REACT_APP_LOCATION || 'http://localhost'
 const port = process.env.LOCATION || 3000
-const url = location.includes("localhost")? `${location}:${port}`: `${location}`;
+const url = location.includes("localhost") ? `${location}:${port}` : `${location}`;
 
 function SavedLogs(props) {
   const [hasBackLog, setbackLog] = useState(false);
+  const [backLogList, setbackLogList] = useState([]);
 
   useEffect(() => {
     const options = {
       credentials: 'include',
     }
     fetch(`${url}/`, options)
-    .then(res => res.json())
-    .then(
+      .then(res => res.json())
+      .then(
         (result) => {
-            console.log(result);
-            if(!result.length)
-              return;  
-            setbackLog(true);
+          console.log(result);
+          if (!result.length)
+            return;
+          setbackLog(true);
+          setbackLogList([...result]);
         },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
         // exceptions from actual bugs in components.
         (error) => {
-            console.log("test");
+          console.log("test");
         }
-    )
-  });
-  if(!hasBackLog){
+      )
+  },[])
+
+
+  if (!hasBackLog) {
     return (
       <div>
         <IntroJumbo />
       </div>
     );
   }
-  else{
-         return (
+  else {
+    return (
       <div>
-        <h1> YOU HAVE STUFF IN BACKLOG:)</h1>
+        <Container >
+          <Row>
+            {backLogList.map((element, i) => {
+              return (
+                <Col sm={12} md={6} lg={4} key={i}>
+                  <LogCard
+                    logTitle={element.logTitle}
+                    logPlot={element.logPlot}
+                    logReleaseDate={element.logReleaseDate}
+                    logPoster={element.logPoster}
+                    logID={element.logID}
+                  />
+                </Col>
+              )
+            })}
+
+          </Row>
+        </Container>
       </div>
     );
   }
