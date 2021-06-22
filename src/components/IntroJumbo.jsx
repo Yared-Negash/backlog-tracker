486e4b851181da1ac5e048569fe123dcc91dc4cf
Yared <yarednegash3115@gmail.com>
2021-06-22 19:14:44 -0400

    add video background to home page

--- src/components/IntroJumbo.jsx
+++ src/components/IntroJumbo.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import backgroundVideo from '../images/backLog_jumbotron_mashup.mp4'
+
+import "./css/introJumbo.css"
 
 function IntroJumbo() {
     return (
-        <Jumbotron>
-            <h1>Is it getting cold? Go check the backLOG</h1>
-            <p>Use this site to track and complete movies, shows, and games in your <b>backLOG</b></p>
-            <p><i>🔥🪓🌲Don't play with fire kids🔥🪓🌲</i></p>
-        </Jumbotron>
+        <div>
+            <video autoPlay muted loop id="myVideo">
+                <source src={backgroundVideo} type="video/mp4" /> 
+            </video>
+            <div class="jumbotronBackground">
+                <h1>Is it getting cold? Go check the backLOG</h1>
+                <p>Use this site to track and complete movies, shows, and games in your <b>backLOG</b></p>
+                <p><i>🔥🪓🌲Don't play with fire kids🔥🪓🌲</i></p>
+            </div>
+        </div>
     )
 }
 
